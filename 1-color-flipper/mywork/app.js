fa31c9d0b3f876bdb88580151124bd5c23452210
Josh Choi <smilecws@gmail.com>
2022-01-17 20:14:17 +0900

    home

--- 1-color-flipper/mywork/app.js
+++ 1-color-flipper/mywork/app.js
@@ -1,17 +1,19 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
 const btn = document.getElementById("btn");
-const colorss = document.querySelectorAll(".color");
+// Get the element with the specified ID:
+// <button class="btn btn-hero" id="btn">Click Me</button>
+const color = document.querySelector(".color");
+// The querySelector() method returns the first element that matches a specified CSS selector(s) in the document.
 
 btn.addEventListener('click', function(){
   // get random number between 0-3
+  // The addEventListener() method attaches an event handler to the document.
   const randomNumber = getRandomNumber();
   console.log(randomNumber);
 
   document.body.style.backgroundColor = colors[randomNumber]
-  colorss.forEach((color) => {
   color.textContent = colors[randomNumber];
-  // Change content of color class
-})
+  // Change content of "color" class not chaning the color
 });
 
 function getRandomNumber(){
