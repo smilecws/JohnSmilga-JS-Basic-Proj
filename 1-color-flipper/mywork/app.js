16378cc93bc8d8752c7c4fd5a1f48a995e0b2c4f
Josh Laptop <smilecws@gmail.com>
2022-01-17 09:03:08 +0900

    final

--- 1-color-flipper/mywork/app.js
+++ 1-color-flipper/mywork/app.js
@@ -1,5 +1,5 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
-// const btn = document.getElementById("btn");
+const btn = document.getElementById("btn");
 const colorss = document.querySelectorAll(".color");
 
 btn.addEventListener('click', function(){
@@ -18,6 +18,6 @@ function getRandomNumber(){
   return Math.floor(Math.random()*colors.length);
   // Math.random() = 0<x<1 random number
   // colors.lentgth = 4
-  // Math.floor up and down
+  // Math.floor down
 
 }
--- 1-color-flipper/mywork/hex.js
+++ 1-color-flipper/mywork/hex.js
@@ -1,6 +1,6 @@
 const hex = [0,1,2,3,4,5,6,7,8,9, "A","B","C","D","E","F"];
 // # f15025
-// const btn = document.getElementById('btn');
+const btn = document.getElementById('btn');
 const color = document.querySelector('.color');
 
 btn.addEventListener('click', function(){
@@ -16,4 +16,6 @@ btn.addEventListener('click', function(){
 
 function getRandomNumber(){
   return Math.floor(Math.random() * hex.length);
+  // The Math.random() method returns a random number from 0 (inclusive) up to but not including 1 (exclusive).
+  // The Math.floor() method rounds a number DOWNWARDS to the nearest integer, and returns the result.
 }
